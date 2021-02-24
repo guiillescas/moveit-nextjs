0673cb0da15a602b695c4ef51c2d0ae5406ee44b
Guilherme Illescas <gui.illescas@gmail.com>
2021-02-24 10:18:35 -0300

    Envolve app with ThemeProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { ReactElement } from 'react';
+import { ThemeProvider } from 'next-themes';
+
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }: any): ReactElement {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider defaultTheme="system">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
