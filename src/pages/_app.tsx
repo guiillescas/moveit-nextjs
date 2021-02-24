7a13f7ba8deb42745a37e02a86b2785c1cecfa19
Guilherme Illescas <gui.illescas@gmail.com>
2021-02-24 16:36:14 -0300

    Insert ChallengeProvider context to the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,14 @@ import React, { ReactElement } from 'react';
 import { ThemeProvider } from 'next-themes';
 
 import '../styles/global.css';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
 function MyApp({ Component, pageProps }: any): ReactElement {
   return (
     <ThemeProvider defaultTheme="system">
-      <Component {...pageProps} />
+      <ChallengesProvider>
+        <Component {...pageProps} />
+      </ChallengesProvider>
     </ThemeProvider>
   );
 }
