915dcc4d30ff123ec23bc62b8286ef457bbbc911
Guilherme Illescas <gui.illescas@gmail.com>
2021-02-28 23:43:54 -0300

    Remove theme switcher

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,9 +1,6 @@
 import React, {
-  ReactElement, useContext, useEffect, useState,
+  ReactElement, useContext,
 } from 'react';
-import { useTheme } from 'next-themes';
-import Switch from 'react-switch';
-import { FiSun, FiMoon } from 'react-icons/fi';
 
 import styles from '../styles/components/ExperienceBar.module.css';
 import { ChallengesContext } from '../contexts/ChallengesContext';
@@ -12,21 +9,6 @@ export function ExperienceBar(): ReactElement {
   const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext);
 
   const percentToNextLevel = Math.round((currentExperience * 100) / experienceToNextLevel);
-
-  const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
-
-  useEffect(() => setMounted(true), []);
-
-  if (!mounted) return null;
-
-  function toogleTheme() {
-    if (theme === 'dark') {
-      setTheme('light');
-    } else {
-      setTheme('dark');
-    }
-  }
 
   return (
     <div className={styles.menu}>
@@ -47,41 +29,6 @@ export function ExperienceBar(): ReactElement {
           xp
         </span>
       </header>
-
-      <div className={styles.switch}>
-        <Switch
-          onChange={() => { toogleTheme(); }}
-          checked={theme === 'dark'}
-          height={20}
-          width={40}
-          handleDiameter={20}
-          checkedIcon={(
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                height: '100%',
-              }}
-            >
-              <FiMoon size={13} color="#fff" />
-            </div>
-          )}
-          uncheckedIcon={(
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                height: '100%',
-              }}
-            >
-              <FiSun size={13} color="#fff" />
-            </div>
-          )}
-          onColor="#547f92"
-        />
-      </div>
     </div>
   );
 }
