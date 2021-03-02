29b8e99471b9a0670457417ec33eb5384511ea41
Guilherme Illescas <gui.illescas@gmail.com>
2021-03-02 16:35:00 -0300

    Transform in function

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
-export default NextAuth({
+export default (req, res) => NextAuth(req, res,{
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
