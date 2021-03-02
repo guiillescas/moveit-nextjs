478c53daaf9eb324ea5587d7d7b3c675214b174e
Guilherme Illescas <gui.illescas@gmail.com>
2021-03-02 16:51:03 -0300

    Add option to signin method

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -3,7 +3,7 @@ import { signIn } from 'next-auth/client';
 
 export default function SignIn(): ReactElement {
   return (
-    <button type="button" onClick={() => signIn('github')}>
+    <button type="button" onClick={() => signIn('github', { redirect_uri: 'https://moveit.guilhermeillescas.dev/' })}>
       Logar
     </button>
   );
