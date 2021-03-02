c9a00ee031fc5ec400f99b438b4ea957bc6d5640
Guilherme Illescas <gui.illescas@gmail.com>
2021-03-02 13:54:22 -0300

    Add session on entire app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { ReactElement } from 'react';
 import { ThemeProvider } from 'next-themes';
+import { useSession } from 'next-auth/client';
+
+import SignIn from './signin';
 
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }: any): ReactElement {
+  const [session] = useSession();
+
   return (
-    <ThemeProvider defaultTheme="system">
-      <Component {...pageProps} />
+    <ThemeProvider>
+      {!session ? <SignIn /> : <Component {...pageProps} session={session} />}
     </ThemeProvider>
   );
 }
