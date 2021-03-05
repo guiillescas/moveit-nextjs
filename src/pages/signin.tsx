e9c0d383ae53d33654fa5b030388ccd96ccb5293
Guilherme Illescas <gui.illescas@gmail.com>
2021-03-05 08:16:00 -0300

    Finish signin page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,10 +1,33 @@
 import React, { ReactElement } from 'react';
 import { signIn } from 'next-auth/client';
 
+import styles from '../styles/pages/Signin.module.css';
+
 export default function SignIn(): ReactElement {
   return (
-    <button type="button" onClick={() => signIn('github', { redirect_uri: 'https://moveit.guilhermeillescas.dev/' })}>
-      Logar
-    </button>
+    <div className={styles.container}>
+      <img src="/giant-logo.svg" alt="Logo pequena moveit" />
+
+      <div className={styles.card}>
+        <img src="/logo-full-white.svg" alt="Logo moveit" />
+
+        <h2>Bem-vindo!</h2>
+
+        <div className={styles.info}>
+          <img src="/github.svg" alt="Logo GitHub" />
+
+          <p>Faça login com seu GitHub para começar</p>
+        </div>
+
+        <button
+          type="button"
+          onClick={() => {
+            signIn('github', { redirect_uri: 'https://moveit.guilhermeillescas.dev/' });
+          }}
+        >
+          Logar
+        </button>
+      </div>
+    </div>
   );
 }
